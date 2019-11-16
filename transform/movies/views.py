fa86b60cebcc2a4cee0436085255461621878c55
oscaruno <dcpereira@inf.ufsm.br>
2019-11-16 20:22:19 -0300

    Documentação Realizada

--- transform/movies/views.py
+++ transform/movies/views.py
@@ -5,8 +5,10 @@ from objectpath import Tree
 movies_bp = Blueprint('movies', __name__)
 
 
-# UTILS
 def getTree():
+    """ Retorna a Tree(objectpath) do arquivo GioMovies
+        Utilizada para todas as queries/pesquisas
+    """
     with open('../files/GioMovies.json', 'r') as file:
         data = json.load(file)
     file.close()
@@ -14,70 +16,109 @@ def getTree():
 
 
 def getBaseName(id, tree):
-    querie = tree.execute('$..topic[@.id is "%s"]' % id.replace('#', '').replace(',', ''))
+    """ Executa uma busca no Tree e retorna o nome do tópico
+        :param: id: Identificação do tópico com o #
+        :param: tree: getTree()
+    """
+    querie = tree.execute('$..topic[@.id is "%s"]' % id.replace('#', ''))
     data = list(querie)[0]
-    if len(data['baseName']) > 1:
-        return data['baseName'][0]['baseNameString']
-    return data['baseName']['baseNameString']
+    try:
+        # Handle do baseName list ou dict
+        if type(data['baseName']) is list:
+            return data['baseName'][0]['baseNameString']
+        return data['baseName']['baseNameString']
+    except:
+        return ''
 
-
-# end UTILS
 
 @movies_bp.route('/')
 @movies_bp.route('/index')
 def index():
+    """ View padrão, retorna uma lista de todos os filmes
+    """
     gio = getTree()
+    # Busca na Tree todos os tópicos do tipo filme
     querie = gio.execute('$..topic[@.instanceOf.topicRef.href is "#Filme"]')
     data = list(querie)
     return render_template("index.html", data=data)
 
 
+def occurrence(ocr, gio):
+    """ Remapeia uma entrada do tipo occurrence
+        :param ocr: ['occurrence']
+        :param gio: getTree()
+    """
+    # Trata basicamente o caso ocr['scope']['topicRef']['href'] = '#site'
+    if 'scope' in ocr:
+        ocr['scope'] = getBaseName(ocr['scope']['topicRef']['href'], gio)
+    elif 'instanceOf' in ocr:
+        ocr['scope'] = getBaseName(ocr['instanceOf']['topicRef']['href'], gio)
+        ocr['resourceData'] = ocr['resourceRef']['href']
+        del ocr['instanceOf']
+        del ocr['resourceRef']
+
+
 @movies_bp.route('/movies/<id>')
 def movies(id):
+    """ View de Filme
+    :param id: Identificador do Filme, sem #
+    :return: informações do filme com associações
+    """
     gio = getTree()
-    querie = gio.execute('$..topic[@.id is "%s"]' % id)
+    # Busca o tópico do parametro informado
+    querie = gio.execute('$..topic[@.id is "%s" and @.instanceOf.topicRef.href is "#Filme"]' % id)
     data = list(querie)
+    # Verifica se o filme existe senão 404
     if len(data) == 1:
         data = data[0]
-        for ocr in data['occurrence']:
-            if 'scope' in ocr:
-                ocr['scope'] = getBaseName(ocr['scope']['topicRef']['href'], gio)
-            elif 'instanceOf' in ocr:
-                ocr['scope'] = getBaseName(ocr['instanceOf']['topicRef']['href'], gio)
-                ocr['resourceData'] = ocr['resourceRef']['href']
-                del ocr['instanceOf']
-                del ocr['resourceRef']
+        # Adapta todas as 'occurrence' para o Jinja2
+        if type(data['occurrence']) is list:
+            for ocr in data['occurrence']:
+                occurrence(ocr, gio)
+        else:
+            # Trata o caso de ter somente uma 'occurrence'
+            occurrence(data['occurrence'], gio)
+            data['occurrence'] = [data['occurrence']]
 
+        # Busca todas as 'association' deste Filme
         querie = gio.execute('$..association["#%s" in @.member..href]' % id)
         association = list(querie)
+        # Adapta as 'association' para o Jinja2
         for ass in association:
             ass['instanceOf'] = getBaseName(ass['instanceOf']['topicRef']['href'], gio)
             member = list(filter(lambda x: x['topicRef']['href'] != ('#' + id), ass['member']))[0]
-            ass['member'] = {'href': member['topicRef']['href'].replace('#', '').replace(',', ''),
+            ass['member'] = {'href': member['topicRef']['href'].replace('#', ''),
                              'val': getBaseName(member['topicRef']['href'], gio)}
         return render_template('movies.html', data=data, asoc=association)
 
-        return jsonify(association)
+        # return jsonify(association)
     else:
         abort(404)
 
 
 @movies_bp.route('/association/<href>')
 def association(href):
+    """ O que está Associado com um tópico
+    :param href: Identificação do tópico sem #
+    :return: A lista de filmes associados com este Tópico
+    """
     gio = getTree()
     querie = gio.execute('$..topic[@.id is "%s"]' % href)
     data = list(querie)
+    # Handle da existência do tópico senão 404
     if len(data) == 1:
         data = data[0]
+        # Realiza a busca do que está associado com o tópico
         querie = gio.execute('$..association["#%s" in @.member..href]' % href)
         association = list(querie)
         for ass in association:
+            # Adapta para o Jinja2
             ass['instanceOf'] = getBaseName(ass['instanceOf']['topicRef']['href'], gio)
             member = list(filter(lambda x: x['topicRef']['href'] != ('#' + href), ass['member']))[0]
-            ass['member'] = {'href': member['topicRef']['href'].replace('#', '').replace(',', ''),
+            ass['member'] = {'href': member['topicRef']['href'].replace('#', ''),
                              'val': getBaseName(member['topicRef']['href'], gio)}
 
         return render_template("association.html", data=data, asoc=association)
-        return jsonify(association)
+        # return jsonify(association)
     else:
         abort(404)
--- xml2json.py
+++ xml2json.py
@@ -1,9 +1,14 @@
+import os
 import json
 from lxml import etree
 from jsonschema import validate
 
 
 def convert(tree):
+    """ Converte de forma genérica qq XML para JSON
+    :param tree: Arquivo XML em lxml.etree
+    :return: JSON
+    """
     XSL = etree.XSLT(etree.parse('files/xml2json.xsl'))
     json_text = XSL(tree)
     data = json.loads(str(json_text))
@@ -11,17 +16,23 @@ def convert(tree):
 
 
 def main():
+    # Carrega o arquivo original para uma representação da lib 'lxml'
     gioxtm = etree.parse("files/GioMovies.xtm")
     data = convert(gioxtm)
+    # Carrega o JSONSchema
     with open("files/GioSchema.json", "r") as file:
         schema = json.load(file)
 
     try:
+        # tenta validar o JSON gerado com seu respectivo Schema
         validate(instance=data, schema=schema)
-        with open("GioMovies.json", "w") as file:
+        # Caso a validação tenha sucesso, grava a informação no arquivo de saída
+        if os.path.exists("files/GioMovies.json"):
+            os.remove("files/GioMovies.json")  # remove se o arquivo já existe
+        with open("files/GioMovies.json", "w") as file:
             json.dump(data, file)
     except:
-        print("Algo deu errado")
+        print("A Validação deu Errado!!!")
 
 
 if __name__ == '__main__':
